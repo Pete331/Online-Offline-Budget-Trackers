7db046daf5aeff34527bad5e09c91d63ef52d54f
Peter Brennan <pweda64@hotmail.com>
2020-07-13 12:39:06 +0800

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,13 @@
-const FILES_TO_CACHE = ["/", "/index.html", "index.js", "styles.css", "db.js"];
+const FILES_TO_CACHE = [
+    "/", 
+    "/index.html", 
+    "/index.js", 
+    "/styles.css", 
+    "/db.js", 
+    "/icons/icon-192x192.png", 
+    "/icons/icon-512x512.png",
+    "/manifest.webmanifest"
+];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
